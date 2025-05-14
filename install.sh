6d07f669e42a581e7b2c3dbb9e9d9966cd72f1a7
Lukhausen <mail@lukhausen.de>
2025-05-15 01:15:19 +0200

    f

--- install.sh
+++ install.sh
@@ -9,23 +9,26 @@ apt-get install -y git wget python3-pip python3-venv
 python3 -m venv vsrenv
 source vsrenv/bin/activate
 
-# 3) Upgrade pip
-pip install --upgrade pip
+# 3) Upgrade pip & wheel
+pip install --upgrade pip wheel
 
-# 4) Install PyTorch + torchvision (match CUDA version; here cu117)
+# 4) **Pin NumPy to 1.x** for OpenCV / mmcv compat
+pip install "numpy<2.0"
+
+# 5) Install PyTorch + torchvision (match your CUDA; here cu117)
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu117
 
-# 5) Install BasicSR (prerequisite)
+# 6) Install BasicSR (prerequisite)
 pip install basicsr
 
-# 6) Clone the official BasicVSR++ repo
+# 7) Clone the official BasicVSR++ repo
 git clone https://github.com/ckkelvinchan/basicvsr_plusplus.git
 cd basicvsr_plusplus
 
-# 7) Install runtime requirements
+# 8) Install runtime requirements (mmcv-full, OpenCV≤4.5.4, etc.)
 pip install -r requirements/runtime.txt
 
-# 8) Download the REDS4 checkpoint (must match demo)
+# 9) Download the REDS4 checkpoint
 mkdir -p checkpoints
 wget -qO checkpoints/basicvsr_plusplus_c64n7_8x1_600k_reds4.pth \
   https://download.openmmlab.com/mmediting/restorers/basicvsr_plusplus/\
